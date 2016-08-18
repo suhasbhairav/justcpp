4e96663d844b0e90517abe11121923d8fad5ed86
Suhas Bhairav <suhasbhairav@gmail.com>
2016-08-18 23:23:24 +0200

    Switch

---
+++ switch.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+	char a = 'b';
+	switch(a){
+		case 'a':
+			cout << " a " << endl;
+			break;
+		case 'b':
+			cout << " b " << endl;
+			break;
+		default:
+			cout << " Just do it! " << endl;
+
+		break;	
+	}
+	return 0;
+}
