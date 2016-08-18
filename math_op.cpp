88c22621e67815e5dd5da376cffbfdc32683cb3a
Suhas Bhairav <suhasbhairav@gmail.com>
2016-08-19 00:32:12 +0200

    Math Operations

---
+++ math_op.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main(){
+	cout << abs(-20) << endl;
+	cout << sin(90) << endl;
+	cout << pow(2,8) << endl;
+	cout << floor(3.2) << endl;
+	cout << sqrt(8) << endl;
+	cout << ceil(3.3) << endl;
+
+	return 0;
+}
