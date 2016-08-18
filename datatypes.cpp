a395998c35b91de2a220846b273d24564fe8df57
Suhas Bhairav <suhasbhairav@gmail.com>
2016-08-19 00:06:39 +0200

    Datatypes

---
+++ datatypes.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+	cout << " Char :" << sizeof(char) << endl;
+	cout << " Int : " << sizeof(int) << endl;
+	cout << "short Int :" << sizeof(short int) << endl;
+	cout << " Long int :" << sizeof(long int) << endl;
+	cout << "Float : " << sizeof(float) << endl;
+	cout << "Double :" << sizeof(double) << endl;
+	cout << "Size of wchar_t" << sizeof(wchar_t) << endl;
+
+	return 0;
+}
